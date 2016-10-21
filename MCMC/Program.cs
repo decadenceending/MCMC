f9355de9a04a87c1a5dcc2960e04e9f21e682326
Orkhan Abdullayev <spectrum_touch@yahoo.com>
2016-10-20 20:30:05 -0400

    -Building in progress

--- MCMC/Program.cs
+++ MCMC/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MCMC
+namespace program
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public void Main(string[] args)
         {
+
+            int i, sum = 0, n;
+            Console.Write("Enter the Nth Number : ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+
+            Console.WriteLine("\nSum of N Numbers : " + sum);
+            Console.ReadLine();
         }
+
+
     }
 }
