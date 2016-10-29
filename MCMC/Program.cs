d822abd09feea10da10d9d6975abfd1e70f2f4f4
Orkhan Abdullayev <spectrum_touch@yahoo.com>
2016-10-28 21:53:46 -0400

    -FIxed simulate Y

--- MCMC/Program.cs
+++ MCMC/Program.cs
@@ -11,7 +11,7 @@ namespace Main_MCMC
     public class MCMC
     {
         ///Declare Variables
-        
+
         int i;
         int IttN;
         int[,] AcceptN;
@@ -30,27 +30,27 @@ namespace Main_MCMC
         double QR;
         int[,] XN;
         int[,] XN1;
-        int[] Y;
-  
+        int[,] Y;
+
         ///Define Theta Method, where double sum is subtracted from single sum
 
-        public double ThetaMethod(int [,] XN,int M, double R)
+        public double ThetaMethod(int[,] XN, int M, double R)
         {
-            return R*XN.Cast<int>().Sum(); 
+            return R * XN.Cast<int>().Sum();
         }
 
         ///Define Ratio of Pi Method
 
-        public double PiRatio(double ThetaI, double ThetaJ,int Temp)
+        public double PiRatio(double ThetaI, double ThetaJ, int Temp)
         {
             return Math.Exp(-(ThetaI - ThetaJ) / Temp);
         }
 
         ///Defina Ratio of q's, q(i|j)/q(j|i)
 
-        public double QRatio(int B,int M,int W)
+        public double QRatio(int B, int M, int W)
         {
-            return (1/W)/((((M*(M-1))/2)-B));
+            return (1 / W) / ((((M * (M - 1)) / 2) - B));
         }
 
         ///Define Random Number Generator Method
@@ -63,13 +63,20 @@ namespace Main_MCMC
 
         ///Calculate alpha value Method
 
-        public double AlphaCalc(double PiR,double QR)
+        public double AlphaCalc(double PiR, double QR)
         {
             return Math.Min(1, PiR * QR);
         }
 
+        ///Define method to sample from XN
+
+        public int RndCoord()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, M);
+        }
         ///Define MCMC Method, with For Loop, and through calling previously defined methods
-        
+
         static void Main()
         {
             ///Define adjustable fixed values of Temp and r
@@ -86,12 +93,13 @@ namespace Main_MCMC
             ///Define Matrix of Weighted Edges.
             ///Currently the array is defined between 3 vertices
             ///
-            int[,] XN = new int[3,3] {
-                { 1, 2, 3 }, 
-                { 4, 5, 6 }, 
+            int[,] XN = new int[3, 3] {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
                 { 7, 8, 9 }};
 
             int[,] XN1 = new int[3, 3];
+            int[,] Y = new int[3,3];
 
             List<int[,]> AcceptN = new List<int[,]>();
 
@@ -99,27 +107,42 @@ namespace Main_MCMC
 
             for (int i = 0; i < IttN; i++)
             {
-                ///Insert simulate Y ~ q(j|XN=i), Y=j portion
-                
+                ///Simulate Y ~ q(j|XN=i), Y=j portion
 
+                int ic = new MCMC().RndCoord();
+                int jc = new MCMC().RndCoord();
+
+                ///Use if statements to check if an element is non-zero or zero.
+
+                if (XN[ic,jc]!=0)
+                {
+                    Y = XN;
+                    Y[ic, jc] = 0;
+                }
+                else
+                {
+                    Y = XN;
+                    Y[ic, jc] = 1;
+
+                }
 
                 ///Call ThetaMethod for Theta I and Theta J
 
-                double ThetaI = new MCMC().ThetaMethod(XN,M,R);
+                double ThetaI = new MCMC().ThetaMethod(XN, M, R);
 
-                double ThetaJ = new MCMC().ThetaMethod(XN,M,R);
+                double ThetaJ = new MCMC().ThetaMethod(Y, M, R);
 
                 ///Call PiRatio Method to be used in alpha calculations
 
-                double PiR = new MCMC().PiRatio(ThetaI,ThetaJ,Temp);
+                double PiR = new MCMC().PiRatio(ThetaI, ThetaJ, Temp);
 
                 ///Call QRatio Method to be used in alpha calculations
 
-                double QR = new MCMC().QRatio(B,M,W);
+                double QR = new MCMC().QRatio(B, M, W);
 
                 ///Call AlphaCalc Method
 
-                double alpha = new MCMC().AlphaCalc(PiR,QR); 
+                double alpha = new MCMC().AlphaCalc(PiR, QR);
 
                 ///Generate random state by calling Rnd Method
 
@@ -136,12 +159,19 @@ namespace Main_MCMC
                 }
                 else
                 {
-                    
+
                 }
 
                 double h = (1 / IttN) * AcceptN.Cast<int>().Sum();///Edit,
-            }
-        }
 
+                ///Generating a graph based on the listed edges connecting vertices
+                
+                
+                
+                ///Plotting the results for a number of trials
+
+            }
+
+        }
     }
 }
